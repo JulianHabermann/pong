d3e86a5b0450a1cd6a2498a39bc9cd507fd1f2eb
JulianHabermann <jhabermann@college.harvard.edu>
2021-01-15 10:46:54 -0600

    deploying public

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+// Default configuration from Babylon
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const appDirectory = fs.realpathSync(process.cwd());
+module.exports = {
+    entry: path.resolve(appDirectory, "src/app.ts"), //path to the main .ts file
+    output: {
+        filename: "js/pongBundle.js", 
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    devServer: {
+        host: "0.0.0.0",
+        port: 8080, //port that we're using for local host (localhost:8080)
+        disableHostCheck: true,
+        contentBase: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
+        publicPath: "/",
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.resolve(appDirectory, "public/index.html"),
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    mode: "development",
+};
