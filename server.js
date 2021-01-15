7f08c9b439d915add5ed41fcd3ab3ad676ec6d31
JulianHabermann <jhabermann@college.harvard.edu>
2021-01-15 15:52:53 -0600

    heroku test 3

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/public'));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(port);
